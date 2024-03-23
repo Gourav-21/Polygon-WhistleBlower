dc755d7d2d0008925a1e2703cbd35e441955d103
Gourav-21 <rahul.dora21@gmail.com>
2024-03-23 06:42:45 +0530

    hardhat config

--- polygon/hardhat.config.ts
+++ polygon/hardhat.config.ts
@@ -3,6 +3,9 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  paths:{
+    artifacts:"../next-frontend/src/artifacts"
+  }
 };
 
 export default config;
