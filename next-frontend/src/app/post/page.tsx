841f6db01969c15262b4d0e7fcf1d07f0d07a463
Gourav-21 <rahul.dora21@gmail.com>
2024-03-24 10:57:14 +0530

    updated the url

--- next-frontend/src/app/post/page.tsx
+++ next-frontend/src/app/post/page.tsx
@@ -1,7 +1,7 @@
 import Post from "./post";
 
 async function getpost() {
-  const res = await fetch('https://secretwhistleblower.vercel.app/api/posts', { next: { tags: ['posts'] }})
+  const res = await fetch('https://polygonwhistleblower.vercel.app/api/posts', { next: { tags: ['posts'] }})
   const data= await res.json();
   return data.posts;
 }
