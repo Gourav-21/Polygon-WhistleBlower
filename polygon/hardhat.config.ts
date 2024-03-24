8526e8a7ecda4e083fc54b5d561e58993c2b21d8
Gourav-21 <rahul.dora21@gmail.com>
2024-03-24 09:47:21 +0530

    config updated

--- polygon/hardhat.config.ts
+++ polygon/hardhat.config.ts
@@ -5,6 +5,12 @@ const config: HardhatUserConfig = {
   solidity: "0.8.24",
   paths:{
     artifacts:"../next-frontend/src/artifacts"
+  },
+  networks:{
+    matic:{
+      url:"https://polygon-mumbai.g.alchemy.com/v2/QuRKdyLAQm9CbmtyQ6IDirTwadluB3Yv",
+      accounts:["91b2ebce2607265ce6cc6c8e80a78ccf32b509b5c7cc803e69af2407a0d2f3bf"],
+    }
   }
 };
 
