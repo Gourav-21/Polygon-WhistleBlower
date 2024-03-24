134598c5f2b1a69826574d07ec4b092d929e94a1
Gourav-21 <rahul.dora21@gmail.com>
2024-03-24 10:50:24 +0530

    error fixed

--- next-frontend/src/components/VoteAndComment.tsx
+++ next-frontend/src/components/VoteAndComment.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { cn } from "@/lib/utils";
 import { postAtomFamily } from "@/store/postAtomFamily";
 import { Vote } from "@/store/vote";
--- next-frontend/src/store/vote.ts
+++ next-frontend/src/store/vote.ts
@@ -2,5 +2,5 @@ import { atom, atomFamily } from "recoil";
 
 export const Vote = atomFamily({
     key: 'voteing',
-    default: date => localStorage.getItem(date),
+    default: date => (typeof window !== 'undefined' ? localStorage.getItem(date) : null),
 });
